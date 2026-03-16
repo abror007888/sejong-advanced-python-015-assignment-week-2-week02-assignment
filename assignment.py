344c96138a20189261ca450af63a5c51cc116c06
abror007888 <abrorr414@gmail.com>
2026-03-16 20:29:04 +0900

    Add files via upload

--- assignment.py
+++ assignment.py
@@ -1,1 +1,29 @@
+# Assignment 1
+length = float(input("Enter length of rectangle: "))
+breadth = float(input("Enter breadth of rectangle: "))
 
+area = length * breadth
+
+print("Area of rectangle:", area)
+
+
+# Assignment 2
+integer_number = 5
+float_number = 3.5
+
+result = integer_number + float_number
+
+print("\nResult:", result)
+
+
+# Assignment 3
+name = input("\nEnter your name: ")
+age = int(input("Enter your age: "))
+gender = input("Enter your gender: ")
+height = float(input("Enter your height in cm: "))
+
+print("\nUser Details")
+print("Name:", name)
+print("Age:", age)
+print("Gender:", gender)
+print("Height:", height, "cm")
